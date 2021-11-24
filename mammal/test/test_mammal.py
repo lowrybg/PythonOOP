779629a015c5a3fb8864531fbdc19b116674d8b1
Taner <lowrybg@users.noreply.github.com>
2021-11-24 20:21:20 +0200

    Mammal testing 80%

--- mammal/test/test_mammal.py
+++ mammal/test/test_mammal.py
@@ -1,5 +1,27 @@
 from unittest import TestCase, main
 
+from project.mammal import Mammal
+
+class TestMammal(TestCase):
+    def setUp(self):
+        self.mammal = Mammal("Joe", 'bird', 'kwak')
+
+    def test_init_creates_all_attributes(self):
+        self.assertEqual('Joe', self.mammal.name)
+        self.assertEqual('bird', self.mammal.type)
+        self.assertEqual('kwak', self.mammal.sound)
+
+    def test_private_mammal_type(self):
+        self.assertEqual("animals", self.mammal._Mammal__kingdom)
+
+    def test_if_return_kingdom(self):
+        result = self.mammal.get_kingdom()
+        self.assertEqual('animals', result)
+
+    def test_if_get_info(self):
+        result = self.mammal.info()
+        self.assertEqual("Joe is of type bird", result)
+
 
 
 if __name__ == '__main__':
