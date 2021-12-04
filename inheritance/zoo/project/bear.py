0e4dede968da6b225b60ee65553a88a8ed6e4517
Taner <lowrybg@users.noreply.github.com>
2021-12-04 19:20:37 +0200

    Inheritance second and third task

---
+++ inheritance/zoo/project/bear.py
@@ -0,0 +1,6 @@
+from project.animal import Animal
+from project.mammal import Mammal
+
+
+class Bear(Mammal):
+    pass
---
+++ inheritance/zoo/project/lizard.py
@@ -0,0 +1,6 @@
+from project.animal import Animal
+from project.reptile import Reptile
+
+
+class Lizard(Reptile):
+    pass
